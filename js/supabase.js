735920aeb437878aefccad54426898d04406ec6d
Mcoeco <109949866+Mcoeco@users.noreply.github.com>
2025-11-06 15:30:40 +0700

    Add files via upload

---
+++ js/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from 'https://esm.sh/@supabase/supabase-js'
+
+export const supabase = createClient(
+  'https://cxsnyqiigdljqrravptt.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN4c255cWlpZ2RsanFycmF2cHR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIzMzA1MzAsImV4cCI6MjA3NzkwNjUzMH0.4P_88ll2-jwkZsFcQKGLAW0ENWY-h97kEyrH5rj0M0E'
+)
